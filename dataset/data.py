cf64e3eab0ca8bf435c930421cdffe4e67ab9153
zutsusemi <zhangzhixing@sjtu.edu.cn>
2022-06-19 14:50:39 +0800

    update

--- dataset/data.py
+++ dataset/data.py
@@ -4,21 +4,21 @@ import numpy as np
 from torch.utils import data
 
 
-def process_dataset(path, step = 30):
+def process_dataset(path, step = 30, output_step = 144):
     train_val = pd.read_csv(path)
     speed = train_val['Wind Speed (m/s)'].to_numpy()
     power_curve = train_val['Theoretical_Power_Curve (KWh)'].to_numpy()
     active_power = train_val['LV ActivePower (kW)'].to_numpy()
     train_val_set = np.c_[speed, power_curve, active_power]
     x, y = [], []
-    for j in range(step, train_val_set.shape[0]):
+    for j in range(step, train_val_set.shape[0] - output_step):
         x.append(train_val_set[j - step : j - 1, :])
-        y.append(train_val_set[j, 2])
+        y.append(train_val_set[j : j + output_step, 2])
     return x, y
 
 class Scada(data.Dataset):
-    def __init__(self, path, step=30):
-        self._x, self._y = process_dataset(path, step)
+    def __init__(self, path, step=30, out_step=144):
+        self._x, self._y = process_dataset(path, step, out_step)
     
     def __len__(self):
         return len(self._y)
--- model/lstm.py
+++ model/lstm.py
@@ -2,6 +2,8 @@ import torch
 import numpy as np
 import torch.nn as nn
 
+__all__ = ('model_lstm', 'Seq2SeqLSTM')
+
 class model_lstm(nn.Module):
     def __init__(self, input_ftr, hidden_ftr, output_ftr, device):
         super(model_lstm, self).__init__()
@@ -19,3 +21,40 @@ class model_lstm(nn.Module):
         return h_out
     # def inference(self, x):
     #     return 1
+
+class Seq2SeqLSTM(nn.Module):
+    def __init__(self, input_ftr, hidden_ftr, output_ftr, output_step=1, device='cpu'):
+        super(Seq2SeqLSTM, self).__init__()
+        self.input_ftr = input_ftr
+        self.hidden_ftr  = hidden_ftr
+        self.output_step = output_step
+        self.output_ftr = output_ftr
+        self.device  = device
+        self.enc = nn.LSTMCell(input_size= input_ftr, hidden_size = hidden_ftr)
+        self.dec = nn.LSTMCell(input_size= hidden_ftr, hidden_size = hidden_ftr)
+        self.linear = nn.Linear(in_features=hidden_ftr, out_features=output_ftr)
+    def forward(self, x):
+        '''
+        x: (L, B, H_input)
+        '''
+        h = torch.zeros(x.shape[1], self.hidden_ftr).to(self.device)
+        c = torch.zeros(x.shape[1], self.hidden_ftr).to(self.device)
+        # _, (h_out, _)  = self.lstm(x, (h0, t0)) # (L, B?, D*H_out = H_out)
+        L, B = x.shape[0], x.shape[1]
+        '''
+        Encoder
+        '''
+        for j in range(L):
+            h, c = self.enc(x[j], (h, c)) # B, H
+        
+        '''
+        Decoder
+        '''
+        pred = []
+        y = h
+        for j in range(self.output_step):
+            h, c = self.dec(y, (h, c))
+            y = h
+            pred.append(self.linear(y)) # B, H_out
+        h_out = torch.stack(pred)
+        return h_out
